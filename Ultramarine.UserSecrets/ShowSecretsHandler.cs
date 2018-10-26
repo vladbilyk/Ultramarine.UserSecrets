53d498904dd513059f30b609e49f9b27184c9985
Vlad Bilyk <vlad.bilyk@gmail.com>
2018-10-27 01:12:21 +0300

    Solution pad context menu was extended by item "Show User Secrets"

--- Ultramarine.UserSecrets/ShowSecretsHandler.cs
+++ Ultramarine.UserSecrets/ShowSecretsHandler.cs
@@ -44,13 +44,6 @@ namespace Ultramarine.UserSecrets
                     // TODO: ask to initalize user secrets
                 }
             }
-
-        }
-
-        protected override void Update(CommandInfo info)
-        {
-            // TODO: check that selected project is c# project
-            info.Enabled = IdeApp.ProjectOperations.CurrentSelectedProject?.FileName != null;
         }
     }
 }
---
+++ Ultramarine.UserSecrets/ShowSecretsMenuHandler.cs
@@ -0,0 +1,15 @@
+﻿using MonoDevelop.Components.Commands;
+using MonoDevelop.Ide;
+
+namespace Ultramarine.UserSecrets
+{
+    public class ShowSecretsMenuHandler : ShowSecretsHandler
+    {
+        protected override void Update(CommandInfo info)
+        {
+            // TODO: check that selected project is c# project
+            info.Enabled = IdeApp.ProjectOperations.CurrentSelectedProject?.FileName != null;
+        }
+    }
+
+}
